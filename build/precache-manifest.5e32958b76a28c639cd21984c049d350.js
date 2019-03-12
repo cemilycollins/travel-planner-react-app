b07a12a45a2ff87ccd5da36b38a385078f35e2c5
cemilycollins <c.emily.collins@gmail.com>
2019-03-12 15:59:54 -0400

    created production build

---
+++ build/precache-manifest.5e32958b76a28c639cd21984c049d350.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ee6c8bc8516b6f80578d",
+    "url": "/static/js/main.9a169399.chunk.js"
+  },
+  {
+    "revision": "53862cd33088103f31a0",
+    "url": "/static/js/2.6c925dc6.chunk.js"
+  },
+  {
+    "revision": "ee6c8bc8516b6f80578d",
+    "url": "/static/css/main.2c15764b.chunk.css"
+  },
+  {
+    "revision": "c314c6d38fe74ff1e8f483df7bf5d423",
+    "url": "/index.html"
+  }
+];
